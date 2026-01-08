a5f5c387ba776484931a7c47d7d5e840146c185b
lgmlgm8786 <lgmlgm8786@gmail.com>
2026-01-08 21:59:47 +0900

    ㅇㅇㅇ

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -38,6 +38,6 @@ _flutter.buildConfig = {"engineRevision":"1e9a811bf8e70466596bcf0ea3a8b5adb5f17f
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "1722134968"
+    serviceWorkerVersion: "1378249180"
   }
 });
