5069083458e349ca19fec07a7abbb2a54a047cf5
lgmlgm8786 <lgmlgm8786@gmail.com>
2026-01-19 01:10:08 +0900

    ㅇㅇ

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"66b42dd1051a980f1e8fb40242947a2897ff1d
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "1353765737" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "3465182895" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
